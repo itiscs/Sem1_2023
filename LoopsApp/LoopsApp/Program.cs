f412b4acc3bea4532ef3619e3067d4f5768c0f4f
itiscs <moudroff@gmail.com>
2023-09-16 12:58:04 +0300

    Циклы 2

--- LoopsApp/LoopsApp/Program.cs
+++ LoopsApp/LoopsApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 5;
+int i;
+double x = 6, p = 1, s = 0;
+
+//i = i + 1;
+//for(i = 0; i < n; i++)
+//{
+//    s = s + x;
+//    p = p * x;
+//}
+
+//i = 0;
+//while(i<n)
+//{
+//    s = s + x;
+//    p = p * x;
+//    i++;
+//}
+
+i = 0;
+do
+{
+    s += x;
+    p = p * x;
+    i++;
+} while (i < n);
+
+
+Console.WriteLine($"s = {s}");
+
+Console.WriteLine($"p = {p}");
+
+int k = 0;
+int a = int.Parse( Console.ReadLine() );
+int b = Math.Abs(a);
+
+do
+{
+    k++;
+    b /= 10;
+} while (b > 0);
+
+Console.WriteLine($"k = {k}");
+
+
+
+
+
+
