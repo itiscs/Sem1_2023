dc7b0dd2cc428fb6efdc8feb96ace43b9a01afc3
itiscs <moudroff@gmail.com>
2023-09-20 16:44:05 +0300

    Сумма посл

---
+++ Posl/Posl/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int a, int b)
+{
+    return a>b ? a : b; 
+}
+
+
+double x = 2.5; //double.Parse(Console.ReadLine());
+
+double s = 0;
+double m = 1;// x^k
+double l = 1;// k!
+
+
+
+for(int k = 1; k < 50; k++)
+{
+    s = s + (m / l);
+    m = m * x;
+    l = l * k;
+}
+
+Console.WriteLine(Max(4,5));
+
+Console.WriteLine(Math.Exp(x));
