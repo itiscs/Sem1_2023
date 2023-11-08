de05796dbcd4cc1a35796b65cda5de23cab8d002
itiscs <moudroff@gmail.com>
2023-11-08 17:07:37 +0300

    умножение матриц

--- MatrixApp/MatrixApp/Program.cs
+++ MatrixApp/MatrixApp/Program.cs
@@ -1,20 +1,56 @@
-﻿int N = 5, M = 10;
-Random r = new Random();
+﻿using System.Data;
 
-int[,] matr = new int[N,M];
+int[,] GenerateMatr(int size)
+{ 
+    Random r = new Random();
+    int[,] matr = new int[size,size];
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            matr[i,j] = r.Next(3);
+    return matr;
+}
 
 
-for (int i = 0; i < N; i++)
+void ShowMatrix(int[,] matr)
 {
-    for (int j = 0; j < M; j++)
-        matr[i,j] = r.Next(100);  
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($"{matr[i, j],3} ");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] MultMatrix(int[,] m1, int[,] m2)
+{
+    if (m1.GetLength(1) != m2.GetLength(0))
+        throw new ArgumentException("Размеры матриц не подходят!");
+
+    var rows = m1.GetLength(0);
+    var cols = m2.GetLength(1);
+    var l = m1.GetLength(1);
+    var res = new int[rows, cols];
+    
+    for(int i = 0; i < rows; i++)
+        for(int j = 0;j < cols; j++)
+            for(int k = 0; k < l; k++)
+                res[i, j] += m1[i, k] * m2[k, j];
+            
+    return res;
 }
 
 
 
-for (int i = 0; i < N; i++)
-{
-    for (int j = 0; j < M; j++)
-        Console.Write($"{matr[i, j],3} ");
-    Console.WriteLine();
-}
+int N = 2;
+
+var a = GenerateMatr(N);
+
+var b = GenerateMatr(N);
+
+
+ShowMatrix(a);
+Console.WriteLine("**********************************");
+ShowMatrix(b);
+Console.WriteLine("**********************************");
+ShowMatrix(MultMatrix(a,b));
